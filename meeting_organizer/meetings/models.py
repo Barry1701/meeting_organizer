80cfd4197a0d00f326d65999b0468e71558791c6
Bartosz Stanczuk <synergy170185@gmail.com>
2024-07-23 16:26:57 +0000

    add ursl to meetings app and style homepage with css

--- meeting_organizer/meetings/models.py
+++ meeting_organizer/meetings/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Room(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
     floor = models.IntegerField(default=0)
     room_number = models.IntegerField()
 
--- meeting_organizer/website/views.py
+++ meeting_organizer/website/views.py
@@ -5,7 +5,7 @@ from meetings.models import Meeting
 # Create your views here.
 def welcome(request):
     return render(request, "website/welcome.html",
-    {"num_meetings": Meeting.objects.count()})
+    {"meetings": Meeting.objects.all()})
 
 def about(request):
     return HttpResponse("I am a dedicated father of two with 15 years of experience in the security environment. Throughout my career, I have developed a strong foundation in maintaining safety and security, ensuring a secure environment for both people and property.Currently, I am expanding my skill set by completing a Full Stack Web Development course at the Code Institute. This new venture has ignited a deep passion for technology and coding within me. I am enthusiastic about combining my extensive experience in security with my newfound skills in web development to pursue innovative and dynamic opportunities in the tech industry")
